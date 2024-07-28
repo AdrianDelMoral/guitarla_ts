8cc9341308a796b4d4ff85569bc9516dbc3f6963
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-07-28 21:41:34 +0200

    Creating a type for the shopping cart, and using it in the shopping cart correctly

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,12 @@ export type Guitar = {
     price: number
 }
 
+export type CartItem = {
+    id: number
+    image: string
+    name: string
+    description: string
+    price: number
+    quantity: number
+}
+
