bfb786f77abb403b0b5ac192aff73a1e42ee7e1e
Adrián Del Moral <adrian.delmoral.martin@gmail.com>
2024-07-29 12:45:08 +0200

    testing the forms in typescript for the types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,11 @@ export type CartItem = Guitar & {
     quantity: number
 }
 
+//* export type CartItem = Pick<Guitar, 'id' | 'name' | 'price'> & { //! pick para seleccionar los atributos de otro type
+//*     quantity: number
+//* }
+
+//* export type CartItem = Omit<Guitar, 'description' | 'image'> & { //! pick para omitir los atributos de otro type
+//*     quantity: number
+//* }
+
